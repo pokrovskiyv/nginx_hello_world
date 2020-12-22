961ac38130a6eb50e5a3ed7a54c8f8ca3f267450
Vitaly <pokrovskiy.v@gmail.com>
2020-12-22 22:55:41 +0700

    Добавлено vote и что-то еще

--- django/nginx_hello_world_project/polls/urls.py
+++ django/nginx_hello_world_project/polls/urls.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('hello/', views.index, name='index'),
-    path('admin/', admin.site.urls),
-    path('maxim/', views.hi_maxim, name='hi_maxim'),
-    path('', views.task, name='')
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
